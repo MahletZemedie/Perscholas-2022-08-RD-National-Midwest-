87f03c7d646ee399136bcdaebd96e33126b4aea6
Mahlet Zemedie <mahletzemedie@gmail.com>
2022-11-28 15:54:40 -0600

    practice assignment strings

---
+++ 303.2.1- Practice Assignment - Strings/JavaStringsIntroduction.java
@@ -0,0 +1,33 @@
+package com.perscholas.java_basics;
+import java.util.Scanner;
+
+/**
+ * @see the question <a href="https://www.hackerrank.com/challenges/java-strings-introduction/problem" </a>
+ */
+public class JavaStringsIntroduction {
+	
+public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first string: ");
+        String A=sc.next();
+        System.out.println("Enter the second  string: ");
+        String B=sc.next();
+        
+        
+        /* Enter your code here. Print output to STDOUT. */
+        int sum = A.length() + B.length();
+        System.out.println(sum);
+        
+        if(A.compareTo(B)>0){
+            System.out.println("Yes");
+        }else System.out.println("No");
+        
+        
+        String str = A.substring(0,1).toUpperCase() + A.substring(1) + " " +
+                    B.substring(0,1).toUpperCase() + B.substring(1) ;
+    
+        System.out.println(str);
+    }
+
+}
