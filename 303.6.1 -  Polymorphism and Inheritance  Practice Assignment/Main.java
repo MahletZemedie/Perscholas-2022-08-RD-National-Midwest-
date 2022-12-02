ee03e76ae6ae670c8de655ca3608cf6afbbd37f8
Mahlet Zemedie <mahletzemedie@gmail.com>
2022-12-02 10:30:23 -0600

    polymorphism and inheritance

---
+++ 303.6.1 -  Polymorphism and Inheritance  Practice Assignment/Main.java
@@ -0,0 +1,96 @@
+package com.perscholas.PolymorphismAndInheritance;
+/*
+ * @author Mahlet Zemedie 
+ * Date 12/2/2022
+ */
+public class Main {
+
+	 public static void main(String[] args) {
+	      // Program at the "interface" defined in the superclass.
+	      // Declare instances of the superclass, substituted by subclasses.
+	      Monster m1 = new FireMonster("r2u2");   // upcast
+	      Monster m2 = new WaterMonster("u2r2");  // upcast
+	      Monster m3 = new StoneMonster("r2r2");  // upcast
+	 
+	      // Invoke the actual implementation
+	      System.out.println(m1.attack());  // Run FireMonster's attack()
+	      System.out.println(m2.attack());  // Run WaterMonster's attack()
+	      System.out.println(m3.attack());  // Run StoneMonster's attack()
+	 
+	      // m1 dies, generate a new instance and re-assign to m1.
+	      m1 = new StoneMonster("a2b2");  // upcast
+	      System.out.println(m1.attack());  // Run StoneMonster's attack()
+	 
+	      // We have a problem here!!!
+	      Monster m4 = new Monster("u2u2");
+	      System.out.println(m4.attack());  // garbage!!!
+	 }
+}
+
+ class Monster {
+	
+String name; 
+	
+	public Monster(String name) {
+		this.name = name;
+	}
+	
+	public String attack() {
+		return "!^_&^$@+%$* I don't know how to attack!";
+	}
+
+}
+ 
+ class FireMonster extends Monster {
+		
+		String name; 
+		
+		public FireMonster(String name) {
+			super(name);
+			
+		}
+		@Override
+		public String attack() {
+			// TODO Auto-generated method stub
+			
+			 return "Attack with fire!";
+			
+		}
+
+	}
+ class StoneMonster extends Monster {
+
+		String name; 
+		
+		public StoneMonster(String name) {
+			super(name);
+			
+		}
+		@Override
+		public String attack() {
+			// TODO Auto-generated method stub
+			
+			return "Attack with Stones!";
+			
+		}
+
+	}
+ class WaterMonster extends Monster {
+
+	String name;
+
+	public WaterMonster(String name) {
+		super(name);
+
+	}
+
+	@Override
+	public String attack() {
+		// TODO Auto-generated method stub
+		
+		return "Attack with water!";
+	}
+
+}
+
+
